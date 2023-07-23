259312908f0b6d2c079b0383300ff094734f0250
Siddhartha-Sunkara <siddhartha.sunkara20@st.niituniversity.in>
2023-07-23 23:06:09 +0530

    change

--- src/app/layout.js
+++ src/app/layout.js
@@ -5,7 +5,7 @@ import { Inter } from 'next/font/google'
 const inter = Inter({ subsets: ['latin'] })
 
 export const metadata = {
-  title: 'Anime Website',
+  title: 'AnimeNexus',
   description: 'Unleash the Power of AnimeNexus - Your Portal to the Enchanting Anime Realm! 🌟 Immerse yourself in a boundless world of anime wonders with our cutting-edge website, delivering the freshest updates, captivating series, and exclusive insights! 🚀 Stay connected to your favorite shows and discover new ones - all in one vibrant hub!',
 }
 
