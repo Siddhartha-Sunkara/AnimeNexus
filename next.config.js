23a26c8c04c6837da480ce31da4e995f5bdd076b
Siddhartha-Sunkara <siddhartha.sunkara20@st.niituniversity.in>
2023-08-17 12:57:52 +0530

    added external links

--- next.config.js
+++ next.config.js
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     images:{
-        domains:["cdn.myanimelist.net" ,'www.google.com' ,'img3.wallspic.com']
+        domains:["c4.wallpaperflare.com","i.pinimg.com","cdn.myanimelist.net" ,'www.google.com' ,'img3.wallspic.com']
     }
 }
 
--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -18,34 +18,34 @@ const Hero = () => {
               <div className="upper flex h-[50%]  gap-10   ">
                 <div className="left w-[50%] bg-red-400 overflow-hidden">
                   <Image
-                    src="/assets/naruto.webp"
+                    src="https://i.pinimg.com/564x/2d/c0/79/2dc079c2a051eb5e022efbf43fe46178.jpg"
                     alt="naruto"
-                    width={200}
-                    height={300}
+                    width={1080}
+                    height={1080}
                     placeholder="blur"
                     blurDataURL="/assets/Eye.webp"
-                    className="w-full "
+                    className="w-full object-cover"
                   />
                 </div>
                 <div className="right w-[50%] bg-red-400 overflow-hidden rounded-full ">
                   <Image
-                    src="/assets/Naruto1.webp"
+                    src="https://i.pinimg.com/564x/36/75/32/3675321bbbb6049461d67291fc44e1ad.jpg"
                     alt="naruto"
-                    width={200}
-                    height={300}
+                    width={1080}
+                    height={1080}
                     placeholder="blur"
                     blurDataURL="/assets/Eye.webp"
-                    className="w-full mt-[-100px]"
+                    className="w-full mt-[-100px] object-cover"
                   />
                 </div>
               </div>
-              <div className="lower h-[50%]  bg-red-600">
+              <div className="lower h-[50%]  bg-black">
                 <Image
-                  src="/assets/naruto2.webp"
+                  src="https://c4.wallpaperflare.com/wallpaper/917/245/80/anime-demon-slayer-kimetsu-no-yaiba-tanjirou-kamado-hd-wallpaper-preview.jpg"
                   alt="naruto"
-                  width={200}
-                  height={300}
-                  className="w-full"
+                  width={1080}
+                  height={1080}
+                  className="object-cover"
                 />
               </div>
             </div>
@@ -58,11 +58,11 @@ const Hero = () => {
             <div className=" slide w-[576px] flex flex-col  gap-5    bg-[#040203]">
               <div className="upper h-[50%] bg-red-600 rounded-full overflow-hidden">
                 <Image
-                  src="/assets/itachi1.webp"
+                  src="https://c4.wallpaperflare.com/wallpaper/693/688/935/uchiha-madara-naruto-shippuuden-wallpaper-preview.jpg"
                   alt="naruto"
-                  width={200}
-                  height={300}
-                  className="w-full "
+                  width={1080}
+                  height={1080}
+                  className="object-cover "
                 />
               </div>
               <div className="lower flex h-[50%]  gap-10   ">
@@ -70,25 +70,25 @@ const Hero = () => {
                   {" "}
                   
                   <Image
-                    src="/assets/Eye.webp"
+                    src="https://i.pinimg.com/564x/50/f7/d7/50f7d7e462e653d28660f0bdf0563239.jpg"
                     alt="naruto"
-                    width={200}
-                    height={300}
+                    width={1080}
+                    height={1080}
                     placeholder="blur"
                     blurDataURL="/assets/Eye.webp"
-                    className="w-full  mt-[-100px]"
+                    className="w-full  mt-[-100px] object-cover"
                   />
                   
                 </div>
                 <div className="right w-[50%] bg-red-400 rounded-s-full overflow-hidden rounded-tr-full">
                   <Image
-                    src="/assets/mob.webp"
+                    src="https://i.pinimg.com/564x/34/6b/5e/346b5e710603122fdc5e38fffef75088.jpg"
                     alt="naruto"
-                    width={200}
-                    height={300}
+                    width={1080}
+                    height={1080}
                     placeholder="blur"
                     blurDataURL="/assets/Eye.webp"
-                    className="w-full"
+                    className="object-cover"
                   />
                 </div>
               </div>
@@ -105,34 +105,34 @@ const Hero = () => {
               <div className="upper flex h-[50%]  gap-10   ">
                 <div className="left w-[50%] bg-red-400 overflow-hidden">
                   <Image
-                    src="/assets/naruto.webp"
+                    src="https://i.pinimg.com/564x/2d/c0/79/2dc079c2a051eb5e022efbf43fe46178.jpg"
                     alt="naruto"
-                    width={200}
-                    height={300}
+                    width={1080}
+                    height={1080}
                     placeholder="blur"
                     blurDataURL="/assets/Eye.webp"
-                    className="w-full "
+                    className="w-full object-cover"
                   />
                 </div>
                 <div className="right w-[50%] bg-red-400 overflow-hidden rounded-full ">
                   <Image
-                    src="/assets/Naruto1.webp"
+                    src="https://i.pinimg.com/564x/36/75/32/3675321bbbb6049461d67291fc44e1ad.jpg"
                     alt="naruto"
-                    width={200}
-                    height={300}
+                    width={1080}
+                    height={1080}
                     placeholder="blur"
                     blurDataURL="/assets/Eye.webp"
-                    className="w-full mt-[-100px]"
+                    className="w-full mt-[-100px] object-cover"
                   />
                 </div>
               </div>
-              <div className="lower h-[50%]  bg-red-600">
+              <div className="lower h-[50%]  bg-black">
                 <Image
-                  src="/assets/naruto2.webp"
+                  src="https://c4.wallpaperflare.com/wallpaper/917/245/80/anime-demon-slayer-kimetsu-no-yaiba-tanjirou-kamado-hd-wallpaper-preview.jpg"
                   alt="naruto"
-                  width={200}
-                  height={300}
-                  className="w-full"
+                  width={1080}
+                  height={1080}
+                  className=" object-cover"
                 />
               </div>
             </div>
@@ -145,35 +145,36 @@ const Hero = () => {
             <div className=" slide w-[576px] flex flex-col  gap-5    bg-[#040203]">
               <div className="upper h-[50%] bg-red-600 rounded-full overflow-hidden">
                 <Image
-                  src="/assets/itachi1.webp"
+                  src="https://c4.wallpaperflare.com/wallpaper/693/688/935/uchiha-madara-naruto-shippuuden-wallpaper-preview.jpg"
                   alt="naruto"
-                  width={200}
-                  height={300}
-                  className="w-full "
+                  width={1080}
+                  height={1080}
+                  className="object-cover "
                 />
               </div>
               <div className="lower flex h-[50%]  gap-10   ">
                 <div className="left w-[50%] bg-red-400 overflow-hidden">
                   {" "}
+                  
                   <Image
-                    src="/assets/Eye.webp"
+                    src="https://i.pinimg.com/564x/50/f7/d7/50f7d7e462e653d28660f0bdf0563239.jpg"
                     alt="naruto"
-                    width={200}
-                    height={300}
+                    width={1080}
+                    height={1080}
                     placeholder="blur"
                     blurDataURL="/assets/Eye.webp"
-                    className="w-full  mt-[-100px]"
+                    className="w-full  mt-[-100px] object-cover"
                   />
+                  
                 </div>
                 <div className="right w-[50%] bg-red-400 rounded-s-full overflow-hidden rounded-tr-full">
                   <Image
-                    src="/assets/mob.webp"
+                    src="https://i.pinimg.com/564x/34/6b/5e/346b5e710603122fdc5e38fffef75088.jpg"
                     alt="naruto"
-                    width={200}
-                    height={300}
+                    width={1080}
+                    height={1080}
                     placeholder="blur"
                     blurDataURL="/assets/Eye.webp"
-                    className="w-full"
                   />
                 </div>
               </div>
@@ -184,7 +185,7 @@ const Hero = () => {
           </div>
         </div>
         <PageWrapper>
-        <div className=" h-[40%] text-white font-['cfour'] w-full text-[50px] md:text-[80px] xl:text-[120px] xl:text-[160px] flex items-center justify-center">
+        <div className=" h-[40%] text-white font-['cfour'] w-full text-[50px] md:text-[80px] lg:text-[120px] xl:text-[160px] flex items-center justify-center">
           <p className=" font-semibold py-20">
             <span className="text-[#7053ff]">A</span>nime
             <span className="text-[#7053ff]">N</span>exus
